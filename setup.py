19d4f476f0fd8d7956342e0e6d83511309759105
Jacob Evarts <jevarts@uoregon.edu>
2020-08-02 16:39:37 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -11,20 +11,31 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Grabs the requirements from requirements.txt
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
 setuptools.setup(
-    name="96-well-compare-jevarts", # Replace with your own username
-    version="0.0.1",
+    name="wellcompare", 
+    version="1.0.0.dev3",
     author="Jacob Evarts",
     author_email="jevarts@uoregon.edu",
     description="A package for doing a well to well analysis of two 96 well plates",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jacobian0208/well_compare.git",
+    url="https://github.com/jacobian0208/well_compare_96",
     packages=setuptools.find_packages(),
+    namespace_packages=setuptools.find_namespace_packages(),
+    install_requires=REQUIREMENTS,  # for more serious requirements then requirements.txt
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    project_urls={
+            'Bug Reports': 'https://github.com/jacobian0208/well_compare_96/issus'
+            }
 )
